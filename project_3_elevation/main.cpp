7bf88d4e270ce88a3cd6523f36e67ba54541cace
Casey Carrington Root <caseyroot97@gmail.com>
2017-12-10 12:54:57 -0600

    Initial Commit

---
+++ project_3_elevation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project_3_elevation
+//
+//  Created by Casey Root on 12/10/17.
+//  Copyright © 2017 Casey Root. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
